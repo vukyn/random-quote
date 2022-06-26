3e4df52c1c318ce009f86172b746b9862128fc0d
Vu Ky <vukynpro@gmail.com>
2022-06-27 02:10:40 +0700

    init commit

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {    
+    return (
+        <div className='footer'>
+            by <a href='https://github.com/vukyn'>Vu Ky</a>
+        </div>
+    )
+};
+
+export default Footer;
